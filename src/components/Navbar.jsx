9946213fa1c08e314323b0cfb309b3db678d4714
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-09-29 18:12:43 +0400

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div>logo</div>
+      <div>
+        <div>ana səhifə</div>
+        <div>haqqında</div>
+        <div>rəsimlər</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
